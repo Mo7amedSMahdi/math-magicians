e35586794be4755706f379f00bf57a4951584ebd
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-05-04 10:41:55 +0300

    add page section title

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -9,78 +9,101 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator flex flex--column">
-      <div className="calculator-header">
-        <p className="result">{state.next || state.total || 0}</p>
+    <sectoin className="calculator-section flex">
+      <h2>Let&apos;s do math!</h2>
+      <div className="calculator flex flex--column">
+        <div className="calculator-header">
+          <p className="result">{state.next || state.total || 0}</p>
+        </div>
+        <div className="calculator-buttons grid">
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            AC
+          </button>
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            +/-
+          </button>
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            %
+          </button>
+          <button
+            type="button"
+            onClick={handleClick}
+            className="btn btn--orang"
+          >
+            &#247;
+          </button>
+
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            7
+          </button>
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            8
+          </button>
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            9
+          </button>
+          <button
+            type="button"
+            onClick={handleClick}
+            className="btn btn--orang"
+          >
+            x
+          </button>
+
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            4
+          </button>
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            5
+          </button>
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            6
+          </button>
+          <button
+            type="button"
+            onClick={handleClick}
+            className="btn btn--orang"
+          >
+            -
+          </button>
+
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            1
+          </button>
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            2
+          </button>
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            3
+          </button>
+          <button
+            type="button"
+            onClick={handleClick}
+            className="btn btn--orang"
+          >
+            +
+          </button>
+
+          <button
+            type="button"
+            onClick={handleClick}
+            className="btn btn--gray span"
+          >
+            0
+          </button>
+          <button type="button" onClick={handleClick} className="btn btn--gray">
+            .
+          </button>
+          <button
+            type="button"
+            onClick={handleClick}
+            className="btn btn--orang"
+          >
+            =
+          </button>
+        </div>
       </div>
-      <div className="calculator-buttons grid">
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          AC
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          +/-
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          %
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--orang">
-          &#247;
-        </button>
-
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          7
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          8
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          9
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--orang">
-          x
-        </button>
-
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          4
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          5
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          6
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--orang">
-          -
-        </button>
-
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          1
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          2
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          3
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--orang">
-          +
-        </button>
-
-        <button
-          type="button"
-          onClick={handleClick}
-          className="btn btn--gray span"
-        >
-          0
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--gray">
-          .
-        </button>
-        <button type="button" onClick={handleClick} className="btn btn--orang">
-          =
-        </button>
-      </div>
-    </div>
+    </sectoin>
   );
 };
 
