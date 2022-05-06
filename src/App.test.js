6316a69d12cfd7b45b20f996e335dfbd4d85dcac
Mo7amedSMahdi <Mo7ammed.salah@outlook.com>
2022-05-06 11:45:00 +0300

    add react component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App.js test', () => {
+  test('Renders', () => {
+    const app = render(<App />);
+
+    expect(app).toMatchSnapshot();
+  });
 });
